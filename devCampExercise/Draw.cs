0e33dd4586763bed793e17ad92bdaaeb7a5ffc60
Boris Mandzhukov <skipter@abv.bg>
2020-02-07 17:26:54 +0200

    Extract methods to another class. Some optimisations.

---
+++ devCampExercise/Draw.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Draw
+{
+    //First line.
+    public static void FirstLine(int number)
+    {
+        Console.WriteLine(new string('-', number) + new string('*', number) + new string('-', number) + new string('*', number) +
+            new string('-', number * 2) + new string('*', number) + new string('-', number) + new string('*', number) +
+            new string('-', number));
+    }
+
+    //Middle part of the logo.
+    public static void Middle(int number)
+    {
+        for (int i = 1; i < (number / 2) + 1; i++)
+        {
+            Console.WriteLine(new string('-', number - i) + new string('*', number + i * 2) + new string('-', number - i * 2) + new string('*', number + i * 2)
+                + new string('-', number - i) + new string('-', number - i) + new string('*', number + i * 2) + new string('-', number - i * 2) + new string('*', number + i * 2)
+                + new string('-', number - i));
+        }
+    }
+
+    //Footer part of the logo.
+    public static void Footer(int number)
+    {
+        for (int i = 0; i < number / 2 + 1; i++)
+        {
+            Console.WriteLine(new string('-', (number / 2) - i) + new string('*', number) + new string('-', i + 1) + new string('-', i) + new string('*', number)
+                + new string('*', number - i * 2 - 1) + new string('-', i + 1) + new string('-', i) + new string('*', number) + new string('-', (number / 2) - i)
+                + new string('-', (number / 2) - i) + new string('*', number) + new string('-', i + 1) + new string('-', i) + new string('*', number)
+                + new string('*', number - i * 2 - 1) + new string('-', i + 1) + new string('-', i) + new string('*', number) + new string('-', (number / 2) - i));
+        }
+    }
+}
+
+
+
--- devCampExercise/MMLogo.cs
+++ devCampExercise/MMLogo.cs
@@ -1,39 +1,16 @@
 ﻿using System;
 
-namespace devCampExercise
+public class MMLogo
 {
-    public class MMLogo
+    public static void Main()
     {
-        public static void Main()
+        int number = Int32.Parse(Console.ReadLine());
+
+        if (number > 2 && number < 10000)
         {
-            int number = int.Parse(Console.ReadLine());
-
-            while (!(number > 2 && number < 10000))
-            {
-                number = int.Parse(Console.ReadLine());
-            }
-
-            //First Line
-            Console.WriteLine(new string('-', number) + new string('*', number) + new string('-', number) + new string('*', number) +
-                new string('-', number * 2) + new string('*', number) + new string('-', number) + new string('*', number) +
-                new string('-', number));
-
-            //Upper part
-            for (int i = 1; i < (number / 2) + 1; i++)
-            {
-                Console.WriteLine(new string('-', number - i) + new string('*', number + i * 2 ) + new string('-', number - i * 2) + new string('*', number + i * 2) 
-                    + new string('-', number - i) + new string('-', number - i) + new string('*', number + i * 2) + new string('-', number - i * 2) + new string('*', number + i * 2)
-                    + new string('-', number - i));
-            }
-
-            //Footer
-            for (int i = 0; i < number / 2 + 1; i++)
-            {
-                Console.WriteLine(new string('-', (number / 2) - i) + new string('*', number) + new string('-', i + 1) + new string('-', i) + new string('*', number) 
-                    + new string('*', number - i * 2 - 1) + new string('-', i + 1) + new string('-', i) + new string('*', number) + new string('-', (number / 2)- i)
-                    + new string('-', (number / 2) - i) + new string('*', number) + new string('-', i + 1) + new string('-', i) + new string('*', number)
-                    + new string('*', number - i * 2 - 1) + new string('-', i + 1) + new string('-', i) + new string('*', number) + new string('-', (number / 2) - i));
-            }
+            Draw.FirstLine(number);
+            Draw.Middle(number);
+            Draw.Footer(number);
         }
     }
 }
